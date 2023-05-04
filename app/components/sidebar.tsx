34c941928baab4d899678486969b8cff85f9d23e
astmating <131445482+astmating@users.noreply.github.com>
2023-05-04 11:24:22 +0800

    sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -151,7 +151,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href='WWW.baidu.com' target="_blank">
+            <a href={REPO_URL}  target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
