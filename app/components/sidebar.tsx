d93df38576be994407e29ae2c85d7e37a89a583e
astmating <131445482+astmating@users.noreply.github.com>
2023-05-04 13:44:57 +0800

    sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -4,7 +4,7 @@ import styles from "./home.module.scss";
 
 import { IconButton } from "./button";
 import SettingsIcon from "../icons/settings.svg";
-import GithubIcon from "../icons/jyrz.png";
+import GithubIcon from "../icons/guanwang.svg";
 import ChatGptIcon from "../icons/chatgpt.svg";
 import AddIcon from "../icons/add.svg";
 import CloseIcon from "../icons/close.svg";
