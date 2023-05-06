d872c3747225891d25e3121272034934d5a50779
astmating <131445482+astmating@users.noreply.github.com>
2023-05-06 15:32:56 +0800

    sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -99,9 +99,9 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>AI研究院-企业专用</div>
         <div className={styles["sidebar-sub-title"]}>
-          <a href="http://test.mpay8.cn/pages/20230506/dmq3GL4Hh9PORGF.html." style={{ textDecoration: 'none' }} >
+          <link to="http://test.mpay8.cn/pages/20230506/dmq3GL4Hh9PORGF.html.">
               欢迎试用ChatGPT,点我咨询更多技巧.
-          </a>
+          </link>
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
