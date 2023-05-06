921f45183d007130dc06def478cfd850d57b7ca0
astmating <131445482+astmating@users.noreply.github.com>
2023-05-06 14:27:04 +0800

    sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,9 +96,11 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>AI研究院</div>
+        <div className={styles["sidebar-title"]}>AI研究院-企业专用</div>
         <div className={styles["sidebar-sub-title"]}>
-          欢迎来到ChatGPT4.0.
+          <Link to="http://test.mpay8.cn/pages/20230506/dmq3GL4Hh9PORGF.html">
+              欢迎试用ChatGPT.
+          </Link>
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
