4e39baa0dc283183be25d24710f34cb62c186474
astmating <131445482+astmating@users.noreply.github.com>
2023-05-06 15:21:57 +0800

    constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -2,7 +2,7 @@ export const OWNER = "jyrz";
 export const REPO = "ChatGPT-Next-Web";
 export const REPO_URL = `http://www.jyrz22.cn/`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-export const Buy_URL = `http://test.mpay8.cn/pages/20230506/cnqggLokL9ooqhF.html`;
+export const Buy_URL = `https://appulfjljvg3856.h5.xiaoeknow.com/p/decorate/homepage?share_user_id=anonymous_A9mkXFpUh8RhxLq8T&wework_share_customer_id=anonymous_A9mkXFpUh8RhxLq8T&entry=2&entry_type=200`;
 export const UPDATE_URL = `https://appulfjljvg3856.h5.xiaoeknow.com/p/decorate/homepage?share_user_id=anonymous_A9mkXFpUh8RhxLq8T&wework_share_customer_id=anonymous_A9mkXFpUh8RhxLq8T&entry=2&entry_type=2002`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
