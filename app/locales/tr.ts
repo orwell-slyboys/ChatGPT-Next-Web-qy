3b7307d5f86485836663dd86f9360ffd42b360b7
astmating <131445482+astmating@users.noreply.github.com>
2023-05-19 10:02:15 +0800

    tr.ts

--- app/locales/tr.ts
+++ app/locales/tr.ts
@@ -5,7 +5,7 @@ const tr: LocaleType = {
   WIP: "Çalışma devam ediyor...",
   Error: {
     Unauthorized:
-      "Yetkisiz erişim, lütfen erişim kodunu ayarlar sayfasından giriniz.",
+      "Click rear blue font [AI application and liquidate] (https://s.pdb2.com/pages/20230518/dn8II0HIagoPqgE.html), the public reply" password "dialog box, you can get the password to use free of charge." ,
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} mesaj`,
