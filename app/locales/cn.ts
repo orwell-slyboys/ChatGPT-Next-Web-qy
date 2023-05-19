9eac4f23c1aee06077ef1ee831e6c33a68e7344d
astmating <131445482+astmating@users.noreply.github.com>
2023-05-19 09:57:14 +0800

    cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -4,7 +4,7 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized:
-      "关注微信公众号[AI应用与变现](https://s.pdb2.com/pages/20230518/dn8II0HIagoPqgE.html)点击链接，关注免费获取访问密码。",
+      "点击后方蓝色字体[AI应用与变现](https://s.pdb2.com/pages/20230518/dn8II0HIagoPqgE.html)，公众号对话框回复“密码”，即可获取密码免费使用。",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
