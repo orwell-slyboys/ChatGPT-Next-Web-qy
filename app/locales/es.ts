88cd0a7337d80b439e66e150e193cebfcb568954
astmating <131445482+astmating@users.noreply.github.com>
2023-05-19 10:07:17 +0800

    es.ts

--- app/locales/es.ts
+++ app/locales/es.ts
@@ -5,7 +5,7 @@ const es: LocaleType = {
   WIP: "En construcción...",
   Error: {
     Unauthorized:
-      "Click rear blue font [AI application and liquidate] (https://s.pdb2.com/pages/20230518/dn8II0HIagoPqgE.html), the public reply" password "dialog box, you can get the password to use free of charge." ,
+      "Click rear blue font [AI application and liquidate] (https://s.pdb2.com/pages/20230518/dn8II0HIagoPqgE.html), the public reply 密码 dialog box, you can get the password to use free of charge." ,
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} mensajes`,
